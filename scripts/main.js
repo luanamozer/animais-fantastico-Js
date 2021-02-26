c38d1a3039b0e9064c6970f4cd9bb84d343fd971
Luana Mozer <luanamozzer1996@gmail.com>
2021-02-25 21:42:20 -0300

    inserindo  função accordion e animações no faq

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,40 @@
-/*
-const linksInternos = document.querySelectorAll('a[href^="#'); // pega todo elemento interno que inicia com href #  
-function handleLink(event){
-  event.preventDefault(); //previne evento padrao não executando envento padrao
-  linksInternos.forEach((link)=>{
-    link.classList.remove('ativo');  //remove a classe ativo do elemento
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if(tabMenu.length && tabContent.length){
+      tabContent[0].classList.add('ativo');
+
+
+      function activeTab(index){
+        tabContent.forEach((section) =>{
+          section.classList.remove('ativo');
+        });
+        tabContent[index].classList.add('ativo');
+      } 
+
+      tabMenu.forEach((itemMenu,index) => {
+        itemMenu.addEventListener('click', () => {
+          activeTab(index);
+        })
+      });
+    }
+}
+initTabNav();
+
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
+  
+  function activeAccordion(){
+     this.classList.toggle('ativo');
+     this.nextElementSibling.classList.toggle('ativo');
+  }
+  
+  accordionList.forEach((item) =>{
+    item.addEventListener('click', activeAccordion);
   });
-  event.currentTarget.classList.add('ativo');  //depois add a class ativo ao elemento clicado
 }
 
-linksInternos.forEach((link)=>{
-  link.addEventListener('click', handleLink);  //add evento de click e chama a função criada acima
-})
-*/
-
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-if(tabMenu.length && tabContent.length){
-  tabContent[0].classList.add('ativo');
-
-
-  function activeTab(index){
-    tabContent.forEach((section) =>{
-      section.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo');
-  } 
-
-  tabMenu.forEach((itemMenu,index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    })
-  });
-
-}
+initAccordion();
