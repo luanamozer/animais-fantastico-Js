b168d0668d5a58fded262feff73069ff3dd8b490
Luana Mozer <luanamozzer1996@gmail.com>
2021-05-09 14:33:32 -0300

    organização do projeto com modules v1.0

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modules/scroll-soft.js';
+import initAnimacaoScroll from './modules/scroll-animation.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-navegation.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
